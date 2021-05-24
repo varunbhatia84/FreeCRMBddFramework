3e81e9eafcac7885957838bc90c8891af13dbbc5
Hridhaan Bhatia <hridhaan@Hridhaans-MacBook-Pro.local>
2021-05-24 15:05:10 +0530

    updated author name

--- src/test/java/stepDefination/LoginStepDefination.java
+++ src/test/java/stepDefination/LoginStepDefination.java
@@ -1,3 +1,4 @@
+//Author : Varun Bhatia
 package stepDefination;
 
 import java.util.concurrent.TimeUnit;
@@ -7,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
